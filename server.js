b75786255b7ffc6fc9241a694f5aec372cd613bd
Rajashree <rajashreedebnath29@gmail.com>
2025-07-13 00:58:28 +0530

    fast picture loading

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
-  static: 'public',
-});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use((req, res, next) => {
-  if (req.url.startsWith('/images/')) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000');
-  }
-  next();
-});
-
 server.use(router);
-
 server.listen(process.env.PORT || 3001, () => {
   console.log('JSON Server is running');
 });
